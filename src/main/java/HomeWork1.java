3b82bd719186a1c5906f0a80ab1f25d52059ca8a
AresGold00 <85446615+AresGold00@users.noreply.github.com>
2021-10-25 22:49:46 +0300

    1. Теперь в каждом задание надо ввести число самостоятельно. 2. Квадратное уравнение. 3. Шифрование слов.

--- src/main/java/HomeWork1.java
+++ src/main/java/HomeWork1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ДЗ1 {
+public class HomeWork1 {
     private static Scanner scanner= new Scanner(System.in);
 
     public static void main (String[] args){
